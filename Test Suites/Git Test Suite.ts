b9085d4e8152a9da0effca91bdf39d5d283df5d6
emmanuel0425 <emmanuel0325@gmail.com>
2020-02-23 16:02:35 -0400

    test message

--- Test Suites/Git Test Suite.ts
+++ Test Suites/Git Test Suite.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>a040eb7e-0c2d-4a65-9ece-5b2b2ce441c2</testSuiteGuid>
+   <testCaseLink>
+      <guid>a6ed004b-588e-4ff0-9409-6ec725260b16</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Kim TC</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5eb1b383-c426-4d0f-95dc-d2c2fa0c0366</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Tom TC</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
